ef2adc8ef2bde43cae339d136f88a5bbb67ed84d
Nnenna Kehinde <nneukamaka@yahoo.com>
2023-03-09 16:35:05 -0800

    Task 3

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -4,7 +4,8 @@ if __name__ == "__main__":
     """Print the addition of all arguments."""
     import sys
 
-args = sys.argv[1:]  # Get all arguments except for the first (which is the script name)
+args = sys.argv[1:]
+# Get all arguments except for the first (which is the script name)
 
 total = 0
 for arg in args:
