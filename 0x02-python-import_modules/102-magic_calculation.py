0daa3d2a43019141c38666c45fc88be38a97f115
Nnenna Kehinde <nneukamaka@yahoo.com>
2023-03-10 13:55:09 -0800

    Task 8

--- 0x02-python-import_modules/102-magic_calculation.py
+++ 0x02-python-import_modules/102-magic_calculation.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 
 def magic_calculation(a, b):
+    """Match bytecode provided"""
     from magic_calculation_102 import add, sub
 
     if a < b:
@@ -9,4 +10,4 @@ def magic_calculation(a, b):
             c = add(c, i)
         return c
     else:
-        return sub(a, b)
+        return (sub(a, b))
