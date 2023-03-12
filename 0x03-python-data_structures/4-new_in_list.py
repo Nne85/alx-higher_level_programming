796592d4a23b85a161920307aa83e25bb2639f32
Nnenna Kehinde <nneukamaka@yahoo.com>
2023-03-12 13:00:06 -0700

    Task 3

--- 0x03-python-data_structures/4-new_in_list.py
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -1,13 +1,10 @@
 #!/usr/bin/puthon3
 
 def new_in_list(my_list, idx, element):
-    # create a copy of the original list
-    new_list = my_list[:]
-
-    # check if the index is within range
-    if idx >= 0 and idx < len(my_list):
-        # replace the element at the given index
-        new_list[idx] = element
-
-    # return the new list
-    return (new_list)
+    if my_list:
+        new_list = my_list[:]
+        if idx < 0 or idx >= len(my_list):
+            return my_list
+        else:
+            new_list[idx] = element
+            return (new_list)
