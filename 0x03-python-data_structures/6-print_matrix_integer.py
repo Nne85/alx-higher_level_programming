ed5ecb664f78355d1be25ebdfdfc7d9ad6f89571
Nnenna Kehinde <nneukamaka@yahoo.com>
2023-03-12 14:21:10 -0700

    Task 5

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -1,12 +1,9 @@
 #!/usr/bin/python3
 def print_matrix_integer(matrix=[[]]):
-    if matrix == [[]]:
+    for row in matrix:
+        for i in range(len(row)):
+            if i == len(row) - 1:
+                print("{:d}".format(row[i]), end="")
+            else:
+                print("{:d}".format(row[i]), end=" ")
         print()
-    else:
-        for row in matrix:
-            for i in range(len(row)):
-                if i != len(row) - 1:
-                    print("{:d}".format(row[i]), end=' ')
-                else:
-                    print("{:d}".format(row[i]))
-
