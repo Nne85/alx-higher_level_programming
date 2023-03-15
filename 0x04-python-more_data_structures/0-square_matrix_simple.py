e50c4dada1502e4a70e1eca60ca8bde08b4e4d91
Nnenna Kehinde <nneukamaka@yahoo.com>
2023-03-15 15:57:42 -0700

    Tasks 0

--- 0x04-python-more_data_structures/0-square_matrix_simple.py
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -1,10 +1,4 @@
-def square_matrix_simple(matrix=[]):
-    """Compute the square value of all integers of a matrix"""
-    # Create a new matrix with the same size as the input
-    result = [[0 for j in range(len(matrix[i]))] for i in range(len(matrix))]
-    # Fill in the values by squaring the corresponding input value
-    for i in range(len(matrix)):
-        for j in range(len(matrix[i])):
-            result[i][j] = matrix[i][j] ** 2
-    return result
+#!/usr/bin/python3
 
+def square_matrix_simple(matrix=[]):
+    return [list(map(lambda x: x ** 2, row)) for row in matrix]
