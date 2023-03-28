fdf054cdfad97662581862b6e2b86663dda09aa5
Nnenna Kehinde <nneukamaka@yahoo.com>
2023-03-28 14:28:35 -0700

    Task 1

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -7,16 +7,17 @@ Function:
     size: calculate the area of a square
 """
 
+
 class Square:
+
     """
     A class representing a square.
 
     Attributes:
     __size (int): The size of the square.
     """
-
     def __init__(self, size):
         """
-        __init__(self, size): Initializes a new instance of the Square class with the given size parameter.
+        __init__(self, size): Initializes the Square class with size.
         """
         self.__size = size
