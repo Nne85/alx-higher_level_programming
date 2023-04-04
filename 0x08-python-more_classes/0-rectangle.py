411359603d675b145c00732c852cd6ea396fa483
Nnenna Kehinde <nneukamaka@yahoo.com>
2023-04-04 06:41:00 -0700

    Task 0

---
+++ 0x08-python-more_classes/0-rectangle.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+
+"""
+This module contains an empty class of a Rectangle that
+defines a rectangle
+
+"""
+
+
+class Rectangle:
+    """This class defines the rectangle"""
+    pass
