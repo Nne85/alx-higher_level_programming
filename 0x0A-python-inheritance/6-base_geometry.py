7d7ffe8ff0c07f9d418e5c0c858e0299298acaa0
Nnenna Kehinde <nneukamaka@yahoo.com>
2023-04-11 15:25:46 -0700

    Raise exception

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,17 @@
+#!/usr/.bin/python3
+"""
+BaseGeometry class module
+"""
+
+
+class BaseGeometry:
+    """
+    BaseGeometry class
+    """
+    def area(self):
+        """
+        Public instance method
+        Raise:
+            Exception with the message area() is not implemented
+        """
+        raise Exception("area() is not implemented")
