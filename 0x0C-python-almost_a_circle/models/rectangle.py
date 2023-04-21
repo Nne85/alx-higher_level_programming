91cd70012cee81b230fcafb6d9b61249127c878c
Nnenna Kehinde <nneukamaka@yahoo.com>
2023-04-21 13:17:33 -0700

    Fixed Test Cases

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -74,7 +74,7 @@ class Rectangle(Base):
             ValueError: Value must be > 0
         """
         if type(value) is not int:
-                raise TypeError("height must be an integer")
+            raise TypeError("height must be an integer")
         if value <= 0:
             raise ValueError("height must be > 0")
         self.__height = value
@@ -106,7 +106,7 @@ class Rectangle(Base):
             ValueError: Value must be >= 0
         """
         if type(value) is not int:
-                raise TypeError("y must be an integer")
+            raise TypeError("y must be an integer")
         if value < 0:
             raise ValueError("y must be >= 0")
         self.__y = value
