2073b7b8edf2bf1a393544037c93556fc85829ac
Nne85 <nneukamaka@yahoo.com>
2023-05-16 17:01:08 -0700

    Fixed

--- 0x0D-SQL_introduction/9-full_creation.sql
+++ 0x0D-SQL_introduction/9-full_creation.sql
@@ -1,6 +1,6 @@
 -- This script creates second table
-CREATE TABLE IF NOT EXISTS second_table (id int, name varchar(256), score int);
-INSERT INTO second_table (id, name, score) VALUE(1, 'John', 10);
-INSERT INTO second_table (id, name, score) VALUE(2, ' Alex', 3);
-INSERT INTO second_table (id, name, score) VALUE(3, 'Bob', 14);
-INSERT INTO second_table (id, name, score) VALUE(4, 'George', 8);
+CREATE TABLE IF NOT EXISTS second_table (id int, name varchar(256),score int);
+INSERT INTO second_table (id, name, score) value (1, 'John', 10);
+INSERT INTO second_table (id, name, score) value (2, 'Alex', 3);
+INSERT INTO second_table (id, name, score) value (3, 'Bob', 14);
+INSERT INTO second_table (id, name, score) value (4, 'George', 8);
