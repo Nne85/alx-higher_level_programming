6950f694fa36ff9c2b3592fb0e289fa9d222e15d
Nnenna Kehinde <nneukamaka@yahoo.com>
2023-06-14 16:26:50 +0000

    Task 6

---
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -0,0 +1,15 @@
+#!/usr/bin/node
+const SquareParent = require('./5-square');
+
+class Square extends SquareParent {
+  charPrint (c) {
+    if (c === undefined) {
+      c = 'X';
+    }
+    const row = c.repeat(this.width);
+    for (let i = 0; i < this.height; i++) {
+      console.log(row);
+    }
+  }
+}
+module.exports = Square;
