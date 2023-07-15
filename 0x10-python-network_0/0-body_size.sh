5f0785e61493a566c2e1a37d8838eb910fcd2e0f
Nne85 <nneukamaka@yahoo.com>
2023-07-15 02:51:34 -0700

    Tasks

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-# Bash script that takes in a URL, sends a request to that URL, and displays
-curl -sI "$1" | awk -F':' '/Content-Length/ {print $2}' | cut -d' ' -f2
+#Bash script that takes in a URL, sends a request t
+curl -s "$1" | wc -c
